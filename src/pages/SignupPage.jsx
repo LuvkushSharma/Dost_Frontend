c15969eb51b43ecc5655051ec51442ae5c8996f4
Luvkush_Sharma <121566305+LuvkushSharma@users.noreply.github.com>
2024-03-27 21:46:29 +0530

    Update SignupPage.jsx

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -83,6 +83,8 @@ const SignupPage = () => {
         title,
       }, { withCredentials: true });
 
+      localStorage.setItem('token', JSON.stringify(response.data.token));
+
       setIsFailed(false);
       setTimerStarts(true);
 
