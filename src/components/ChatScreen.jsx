ec1fd7e2df8a26351232dfa8d42672fed9262509
Luvkush_Sharma <121566305+LuvkushSharma@users.noreply.github.com>
2024-04-09 10:38:39 +0530

    Update ChatScreen.jsx

--- src/components/ChatScreen.jsx
+++ src/components/ChatScreen.jsx
@@ -315,9 +315,13 @@ const ChatScreen = ({ selectedFriend, sender }) => {
         receiverId = sender.senderId;
       }
 
+      console.log("handle delete starts");
+
       const res = await axios.delete(`${baseUrl}/api/v1/users/chats/delete`, {
         data: { senderId, receiverId, message },
       }, { withCredentials: true });
+
+      console.log("handle delete ends");
 
       if (res.status === 204) {
         if (senderId === sender.senderId) {
