87b6daf1d7758f5a8e1d9d7ca59f330a1d8a7fe3
Luvkush_Sharma <121566305+LuvkushSharma@users.noreply.github.com>
2024-04-13 08:55:15 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -63,10 +63,7 @@ function Navbar() {
 
   const handleLogout = async () => {
     try {
-      const res = await axios.post(`${baseUrl}/api/v1/users/logout`, { headers: {
-        'Access-Control-Allow-Origin': '*', 
-        'Content-Type': 'application/json'
-    },withCredentials: true });
+      const res = await axios.post(`${baseUrl}/api/v1/users/logout`, {withCredentials: true});
       navigate("/", { replace: true });
     } catch (error) {
       console.error(
