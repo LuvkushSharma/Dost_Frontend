5d20ec9a9ac17d357645630a9ae1e18899654e9d
Luvkush_Sharma <121566305+LuvkushSharma@users.noreply.github.com>
2024-07-24 21:40:33 +0530

    Update Wave.jsx

--- src/components/Wave/Wave.jsx
+++ src/components/Wave/Wave.jsx
@@ -10,10 +10,10 @@ const Wave = () => {
             <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
           </defs>
           <g className="parallax">
-            <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7)" />
-            <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
-            <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
-            <use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff" />
+            <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(0,0,0,0.7)" />
+            <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(0,0,0,0.5)" />
+            <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(0,0,0,0.3)" />
+            <use xlinkHref="#gentle-wave" x="48" y="7" fill="#000" />
           </g>
         </svg>
       </div>
