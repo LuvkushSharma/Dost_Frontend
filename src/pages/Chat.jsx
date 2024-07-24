b1918fbc93ad03de623a56bd6cfdaeeac0e9aa9c
Luvkush_Sharma <121566305+LuvkushSharma@users.noreply.github.com>
2024-07-24 21:03:59 +0530

    Update Chat.jsx

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -9,18 +9,38 @@ const Container = styled.div`
   padding: 0;
   box-sizing: border-box;
   display: flex;
-  width: 100vw; /* Viewport width */
-  height: 100vh; /* Viewport height */
+  width: 100vw;
+  height: 100vh;
+  background-color: #121212;
 `;
 
 const FriendListContainer = styled.div`
-  flex: 0 0 20vw; /* 30% width */
-  background-color: #c3cfe2;
+  flex: 0 0 20vw;
+  background-color: #1f1f1f;
+  border-right: 1px solid #444;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const ChatScreenContainer = styled.div`
-  flex: 0 0 80vw; /* 70% width */
-  background-color: #1a759f;
+  flex: 0 0 80vw;
+  background-color: #121212;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Header = styled.div`
+  width: 100%;
+  padding: 10px;
+  background: linear-gradient(90deg, #000000 0%, #434343 100%);
+  color: #ffffff;
+  text-align: center;
+  font-size: 1.5rem;
+  border-bottom: 1px solid #444;
 `;
 
 const Chat = () => {
@@ -34,6 +54,7 @@ const Chat = () => {
   return (
     <Container>
       <FriendListContainer>
+        <Header>Friends</Header>
         <FriendList onSelectFriend={handleFriendSelect} setSender={setSender} />
       </FriendListContainer>
       <ChatScreenContainer>
