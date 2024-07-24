ce5fb0049c1149fd0605a732f5232efabec79313
Luvkush_Sharma <121566305+LuvkushSharma@users.noreply.github.com>
2024-07-24 21:03:33 +0530

    Update FriendsList.jsx

--- src/components/FriendsList.jsx
+++ src/components/FriendsList.jsx
@@ -2,12 +2,27 @@ import React from 'react';
 import { useEffect } from "react";
 import axios from "axios";
 import { useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+// Keyframes for animations
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
 
 const FriendListContainer = styled.div`
   max-width: 100%;
-  background: linear-gradient(90deg, #00C9FF 0%, #92FE9D 100%);
+  background: #000;
   overflow-y: auto;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(255, 255, 255, 0.1);
 `;
 
 const FriendItem = styled.div`
@@ -15,11 +30,16 @@ const FriendItem = styled.div`
   align-items: center;
   padding: 10px;
   cursor: pointer;
-  transition: background-color 0.3s ease;
-  border: 1px solid #495057;
-
+  transition: transform 0.3s ease, background-color 0.3s ease;
+  border: 1px solid #fff;
+  margin-bottom: 10px;
+  border-radius: 10px;
+  background: #1a1a1a;
+  animation: ${fadeIn} 0.5s ease-in-out;
+  
   &:hover {
-    background-color: #adb5bd;
+    background-color: #333;
+    transform: scale(1.05);
   }
 `;
 
@@ -28,29 +48,37 @@ const Avatar = styled.img`
   height: 50px;
   border-radius: 50%;
   margin-right: 10px;
+  border: 2px solid #fff;
 `;
 
-const FriendInfo = styled.div``;
+const FriendInfo = styled.div`
+  color: #fff;
+`;
 
 const FriendName = styled.h3`
   margin: 0;
   font-size: 16px;
+  color: #fff;
 `;
 
 const FriendTitle = styled.p`
   margin: 0;
   font-size: 14px;
+  color: #ccc;
 `;
 
 const FriendList = ({ onSelectFriend, setSender }) => {
   const [friendsList, setFriendsList] = useState([]);
 
-  const baseUrl = "https://dost-backend.onrender.com";
+  const baseUrl = "http://localhost:3000";
 
   useEffect(() => {
     const fetchFriendRequests = async () => {
       try {
-        const res = await axios.get(`${baseUrl}/api/v1/users/friendsList`, { withCredentials: true });
+        const res = await axios.get(`${baseUrl}/api/v1/users/friendsList`, {headers: {
+          'Access-Control-Allow-Origin': '*', 
+          'Content-Type': 'application/json'
+      }, withCredentials: true });
 
         const senderData = res.data.data.filter(
           (friend) =>
