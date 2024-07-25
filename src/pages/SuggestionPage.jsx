487704a176b75d9ce308cc841d4073add387259f
Luvkush_Sharma <121566305+LuvkushSharma@users.noreply.github.com>
2024-07-25 07:38:42 +0530

    Update SuggestionPage.jsx

--- src/pages/SuggestionPage.jsx
+++ src/pages/SuggestionPage.jsx
@@ -7,7 +7,7 @@ import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import AlertComponent from "./../components/AlertComponent";
 import Loader from "./../components/Loader";
-import { Avatar, Chip } from "@mui/material"; // Import additional components
+import { Avatar, Chip } from "@mui/material";
 import { Box } from "@mui/system";
 
 const SuggestionPage = () => {
@@ -23,7 +23,13 @@ const SuggestionPage = () => {
     const fetchSuggestions = async () => {
       try {
         setLoading(true);
-        const res = await axios.get(`${baseUrl}/api/v1/users/suggest`, { withCredentials: true });
+        const res = await axios.get(`${baseUrl}/api/v1/users/suggest`, {
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json'
+          },
+          withCredentials: true
+        });
         const usersArray = Object.values(res.data.data);
         setSuggestedUsers(usersArray);
         setLoading(false);
@@ -37,9 +43,13 @@ const SuggestionPage = () => {
 
   const handleRequestFriendship = async (userId) => {
     try {
-      const res = await axios.post(`${baseUrl}/api/v1/users/friend-request`, { userId }, { withCredentials: true });
-
-      console.log("Friend Request kaise ho : ", res.data);
+      const res = await axios.post(`${baseUrl}/api/v1/users/friend-request`, { userId }, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json'
+        },
+        withCredentials: true
+      });
 
       if (res.data.success !== false) {
         setRequestSentSuccess(true);
@@ -53,7 +63,13 @@ const SuggestionPage = () => {
 
   const handleRemoveFromSuggestions = async (userId) => {
     try {
-      const res = await axios.delete(`${baseUrl}/api/v1/users/suggest/${userId}`, { withCredentials: true });
+      const res = await axios.delete(`${baseUrl}/api/v1/users/suggest/${userId}`, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json'
+        },
+        withCredentials: true
+      });
       const updatedSuggestions = suggestedUsers.filter(
         (user) => user._id !== userId
       );
@@ -64,12 +80,12 @@ const SuggestionPage = () => {
   };
 
   return (
-    <Box sx={{ background: "linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(45,178,253,1) 100%)", minHeight: "100vh" }}>
+    <Box sx={{ background: "linear-gradient(to bottom right, #00203FFF, #ADEFD1FF)", minHeight: "100vh", padding: "20px" }}>
       <Typography
         variant="h3"
         align="center"
         gutterBottom
-        sx={{ fontWeight: "bold", color: "#343a40", padding: "20px" }}
+        sx={{ fontWeight: "bold", color: "#FFF", padding: "20px" }}
       >
         Connect with potential friends!
       </Typography>
@@ -94,7 +110,7 @@ const SuggestionPage = () => {
           <Loader />
         ) : suggestedUsers.length === 0 ? (
           <Box>
-            <Typography variant="h6" component="h2" align="center">
+            <Typography variant="h6" component="h2" align="center" sx={{ color: "#FFF" }}>
               Oops! It seems like our suggestion box is feeling a bit shy today.{" "}
               <br />
               Don't worry, you can still have a great time with your existing friends!
@@ -104,9 +120,9 @@ const SuggestionPage = () => {
           <Grid container spacing={3}>
             {suggestedUsers.map((user) => (
               <Grid item xs={12} sm={6} md={4} lg={3} key={user._id}>
-                <Card sx={{ backgroundColor: "#168aad", borderRadius: 4 }}>
+                <Card sx={{ backgroundColor: "#1c4e80", borderRadius: 4, boxShadow: "0 4px 12px rgba(0,0,0,0.2)" }}>
                   <CardContent
-                    sx={{ display: "flex", flexDirection: "column" }}
+                    sx={{ display: "flex", flexDirection: "column", color: "#FFF" }}
                   >
                     <div style={{ display: "flex", alignItems: "center" }}>
                       <Avatar
@@ -114,14 +130,14 @@ const SuggestionPage = () => {
                           user.cloudinaryImageUrl
                             ? user.cloudinaryImageUrl
                             : "/default-avatar.png"
-                        } // Handle missing profile picture
+                        }
                         sx={{ width: 60, height: 60, marginRight: "10px" }}
                       />
                       <div>
-                        <Typography variant="h6" component="h2">
+                        <Typography variant="h6" component="h2" sx={{ color: "#FFF" }}>
                           {user.name}
                         </Typography>
-                        <Typography variant="body2" component="p">
+                        <Typography variant="body2" component="p" sx={{ color: "#EEE" }}>
                           {user.email}
                         </Typography>
                         {user.participatedInHackathon && (
@@ -129,7 +145,7 @@ const SuggestionPage = () => {
                             label="Hackathon Participant"
                             variant="outlined"
                             size="small"
-                            sx={{ margin: "5px" }}
+                            sx={{ margin: "5px", color: "#FFF", borderColor: "#FFF" }}
                           />
                         )}
                       </div>
@@ -139,11 +155,25 @@ const SuggestionPage = () => {
                         display: "flex",
                         justifyContent: "space-between",
                         marginTop: "auto",
+                        gap: "10px"
                       }}
                     >
                       <Button
                         variant="contained"
-                        sx={{ background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 0%, rgba(0,212,255,1) 100%)", color: "#f5ebe0" }}
+                        size="small"
+                        sx={{
+                          background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 0%, rgba(0,212,255,1) 100%)",
+                          color: "#FFF",
+                          boxShadow: "0 4px 8px rgba(0,0,0,0.3)",
+                          transition: "transform 0.3s, background-color 0.3s",
+                          "&:hover": {
+                            backgroundColor: "#555",
+                            transform: "scale(1.1)",
+                          },
+                          "&:active": {
+                            transform: "scale(0.9)",
+                          },
+                        }}
                         onClick={() => {
                           handleRequestFriendship(user._id);
                           setName(user.name);
@@ -153,10 +183,17 @@ const SuggestionPage = () => {
                       </Button>
                       <Button
                         variant="outlined"
-                        sx={{ background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,119,121,1) 0%, rgba(0,212,255,1) 100%)", color: "#f5ebe0" }}
+                        size="small"
+                        sx={{
+                          borderColor: "#FFF",
+                          color: "#FFF",
+                          "&:hover": {
+                            borderColor: "#555",
+                          },
+                        }}
                         onClick={() => handleRemoveFromSuggestions(user._id)}
                       >
-                        Remove from Suggestions
+                        Remove
                       </Button>
                     </div>
                   </CardContent>
